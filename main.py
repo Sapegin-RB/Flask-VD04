556d18c6ddb1d9353deaa7a1e871d502ab836987
Sapegin-RB <sapeginskal@mail.ru>
2024-11-12 14:21:51 +0700

    Files for launching sites using Flask

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    # Получение текущей даты и времени
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return f"<h1>Текущие дата и время: {current_time}</h1>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
